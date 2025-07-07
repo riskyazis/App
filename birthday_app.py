5c2cddbe52d48d54ed8e35d958b23761d04e29a9
riskyazis <126452678+riskyazis@users.noreply.github.com>
2025-07-07 10:14:16 +0800

    Update birthday_app.py
    Perbaikan

--- birthday_app.py
+++ birthday_app.py
@@ -6,9 +6,9 @@ st.set_page_config(page_title="Ucapan Ulang Tahun", layout="centered")
 st.markdown(
     """
     <div style="text-align: center;">
-        <img src="https://i.pinimg.com/originals/e9/e0/35/e9e03538f6ab69c626cfbdb8cc7bc4a3.gif"
+        <img src="https://pinus.florist/wp-content/uploads/2023/08/Garbera-dan-Tagkai.jpg"
              style="width: 300px; border-radius: 15px; box-shadow: 0 0 10px rgba(0,0,0,0.3);" />
-        <h3>Petunjuk ada di sini... 😏</h3>
+        <h3>3 Bunga Garbera</h3>
     </div>
     """,
     unsafe_allow_html=True
@@ -43,10 +43,10 @@ if password_input:
             ">
                 <h2>🎉 Selamat Ulang Tahun! 🎂</h2>
                 <p style="font-size: 18px;">
-                    Hai <b>__________</b>,<br><br>
-                    Di usia yang ke-<b>___</b> ini, semoga kamu selalu sehat, bahagia, dan sukses dalam setiap langkahmu!<br><br>
-                    Dengan cinta,<br>
-                    <b>__________</b>
+                    Hai <b>Nabilla Salsabila Noor</b>,<br><br>
+                    Di usia yang ke-<b>22 tahun</b> ini, semoga kamu selalu sehat, bahagia, dan sukses dalam setiap langkahmu!<br><br>
+                    Pacarmu,<br>
+                    <b>Risky</b>
                 </p>
             </div>
             """,
